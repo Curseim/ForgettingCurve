78bc659396e1812d60b7cb349e59da217c9c7d9f
Curseim <curseim4636@gmail.com>
2025-05-16 10:03:32 +0900

    Fix: DataEntryModel의 FirstEntry 람다 표현식의 값이 파일에 저장되지 않게 수정

--- ForgettingCurve/Class/Data/DataEntryModel.cs
+++ ForgettingCurve/Class/Data/DataEntryModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,7 +17,7 @@ namespace ForgettingCurve.Class.Data
         public string Title { get; set; }
         public string Contents { get; set; }
 
-
+        [JsonIgnore]
         public DateTime FirstEntry => DateTime.Parse(FirstEntryTime);
     }
 
